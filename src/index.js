dfae0334c618dde368385991a55953a93ac6241e
Nagulan <nagulan77fr@icloud.com>
2023-06-27 20:47:37 +0200

    added little changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import { app } from "./server/index.js";
 import { http_port } from "./env.js";
 import { Database } from "./database/index.js";
-import { randomBytes } from "crypto";
-import { init } from "./init/index.js"
 const database = new Database();
 const start = async () => {
-    await Database.ping();
-    await database.connect();
-    await init();
-    app.listen(http_port, () => {
-        console.log(`server start in port ${http_port}`);
-    });
-}
+  await Database.ping();
+  await database.connect();
+  app.listen(http_port, () => {
+    console.log(`server start in port ${http_port}`);
+  });
+};
 start();
